77f955870bfaa0cd0087d80cb8af40c639dffac6
Kazmouzm <96433355+Kazmouzm@users.noreply.github.com>
2025-04-07 22:06:19 +0200

    delet name_question

--- lib/question/name2.dart
+++ lib/question/name2.dart
@@ -1,67 +1,67 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_sdg/question/gettingStarted3.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_sdg/question/gettingStarted3.dart';
 
-class NamePage extends StatelessWidget {
-  const NamePage({Key? key}) : super(key: key);
-  Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: const Color(0xff040324),
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          backgroundColor: const Color(0xff040324),
-          title: Align(
-            alignment: Alignment.centerRight,
-            child: TextButton(
-              onPressed: () => Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const GettingStartedPage(),
-                ),
-              ),
-              child: const Text(
-                'skip',
-                style: TextStyle(color: Color(0xff3BBE6B)),
-              ),
-            ),
-          ),
-        ),
-        body: Column(children: [
-          const SizedBox(height: 20),
-          RichText(
-              text: const TextSpan(
-                  style: TextStyle(fontSize: 30),
-                  children: <TextSpan>[
-                TextSpan(
-                    text: ' But enough about me,\n what is ',
-                    style: TextStyle(color: Colors.white)),
-                TextSpan(
-                  text: 'your name',
-                  style: TextStyle(
-                    color: Color(0xff3BBE6B),
-                    fontStyle: FontStyle.italic,
-                  ),
-                ),
-                TextSpan(text: '?', style: TextStyle(color: Colors.white)),
-              ])),
-          const SizedBox(
-            height: 460,
-          ),
-          Align(
-            alignment: FractionalOffset.bottomCenter,
-            child: TextButton(
-              child: const Text('Continue',
-                  style: TextStyle(
-                      color: Color(0xff3BBE6B),
-                      fontFamily: 'OswaldRegular',
-                      fontSize: 30)),
-              onPressed: () => Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => GettingStartedPage(),
-                ),
-              ),
-            ),
-          ),
-        ]));
-  }
-}
+// class NamePage extends StatelessWidget {
+//   const NamePage({Key? key}) : super(key: key);
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         backgroundColor: const Color(0xff040324),
+//         appBar: AppBar(
+//           automaticallyImplyLeading: false,
+//           backgroundColor: const Color(0xff040324),
+//           title: Align(
+//             alignment: Alignment.centerRight,
+//             child: TextButton(
+//               onPressed: () => Navigator.push(
+//                 context,
+//                 MaterialPageRoute(
+//                   builder: (context) => const GettingStartedPage(),
+//                 ),
+//               ),
+//               child: const Text(
+//                 'skip',
+//                 style: TextStyle(color: Color(0xff3BBE6B)),
+//               ),
+//             ),
+//           ),
+//         ),
+//         body: Column(children: [
+//           const SizedBox(height: 20),
+//           RichText(
+//               text: const TextSpan(
+//                   style: TextStyle(fontSize: 30),
+//                   children: <TextSpan>[
+//                 TextSpan(
+//                     text: ' But enough about me,\n what is ',
+//                     style: TextStyle(color: Colors.white)),
+//                 TextSpan(
+//                   text: 'your name',
+//                   style: TextStyle(
+//                     color: Color(0xff3BBE6B),
+//                     fontStyle: FontStyle.italic,
+//                   ),
+//                 ),
+//                 TextSpan(text: '?', style: TextStyle(color: Colors.white)),
+//               ])),
+//           const SizedBox(
+//             height: 460,
+//           ),
+//           Align(
+//             alignment: FractionalOffset.bottomCenter,
+//             child: TextButton(
+//               child: const Text('Continue',
+//                   style: TextStyle(
+//                       color: Color(0xff3BBE6B),
+//                       fontFamily: 'OswaldRegular',
+//                       fontSize: 30)),
+//               onPressed: () => Navigator.push(
+//                 context,
+//                 MaterialPageRoute(
+//                   builder: (context) => GettingStartedPage(),
+//                 ),
+//               ),
+//             ),
+//           ),
+//         ]));
+//   }
+// }
